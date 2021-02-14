912c7da73b6d73b40876ce2b23fa4937be416ab0
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-02-13 19:03:05 -0600

    workout

--- models/workout.js
+++ models/workout.js
@@ -47,14 +47,3 @@ const workoutSchema = new Schema(
   }
 );
 
-// adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function() {
-  // "reduce" array of exercises down to just the sum of their durations
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);
-});
-
-const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = Workout;
